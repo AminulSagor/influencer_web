57105173b3e1e8045e82c14eb9c1b918ca623de2
ninjastorm24 <siamabdullah1100@gmail.com>
2025-12-23 12:43:05 +0600

    reports

---
+++ app/[locale]/(agency)/agency/reports/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReportCard from "./_components/report-card";
+
+const page = () => {
+  return (
+    <div className="p-4">
+      <ReportCard />
+    </div>
+  );
+};
+
+export default page;
