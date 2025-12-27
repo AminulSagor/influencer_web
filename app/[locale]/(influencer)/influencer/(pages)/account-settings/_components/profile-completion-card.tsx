d95b046be6ae1c1111b8099730a8fe7efec13dbc
Shek Rasel <swe.rasel@gmail.com>
2025-12-27 09:13:42 +0600

    modify

--- app/[locale]/(influencer)/influencer/(pages)/account-settings/_components/profile-completion-card.tsx
+++ app/[locale]/(influencer)/influencer/(pages)/account-settings/_components/profile-completion-card.tsx
@@ -1,34 +1,40 @@
-import { CheckCircle, Pencil } from "lucide-react";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { CheckCircle, SquarePen } from "lucide-react";
+import Link from "next/link";
 
 export default function ProfileCompletionCard() {
   return (
-    <div className="rounded-2xl border bg-white p-6 h-full flex flex-col">
-      {/* Header */}
-      <div className="flex items-center gap-2 mb-4">
-        <CheckCircle className="w-5 h-5 text-[#2D5016]" />
-        <h3 className="text-lg font-semibold text-[#2D5016]">
-          Profile Completion
-        </h3>
-      </div>
-
-      {/* Progress bar */}
-      <div className="w-full h-2 rounded-full bg-[#E6ECD9] mb-6">
-        <div className="h-full w-[60%] rounded-full bg-[#5A7D3B]" />
-      </div>
-
-      {/* Bio */}
-      <div className="rounded-xl border p-4 flex-1">
-        <div className="flex items-center justify-between mb-2">
-          <p className="font-medium text-[#2D5016]">Bio</p>
-          <Pencil className="w-4 h-4 text-[#6B7A4C] cursor-pointer" />
+    <Card className="border-none">
+      <CardHeader>
+        <div className="flex items-center gap-2">
+          <CheckCircle className="w-5 h-5 text-[#2D5016]" />
+          <h3 className="text-lg font-semibold text-[#2D5016]">
+            Profile Completion
+          </h3>
         </div>
+        {/* Progress bar */}
+        <div className="w-full h-2 rounded-full bg-[#E6ECD9]">
+          <div className="h-full w-[60%] rounded-full bg-[#5A7D3B]" />
+        </div>
+      </CardHeader>
 
-        <p className="text-sm text-gray-500 leading-relaxed">
-          The Authority In Fashion & Lifestyle Marketing. With Deep Industry
-          Connections And A Passion For Aesthetics, We Place Your Brand At The
-          Center.
-        </p>
-      </div>
-    </div>
+      <CardContent>
+        {/* Bio */}
+        <Link href={"/influencer/account-settings/verification-checklist"}>
+          <div className="rounded-xl border p-4 flex-1 space-y-2">
+            <div className="flex gap-2 items-center">
+              <p className="font-medium text-[#2D5016]">Bio</p>
+              <SquarePen className="cursor-pointer text-dark-gray" size={14} />
+            </div>
+
+            <p className="text-sm text-gray-500 leading-relaxed">
+              The Authority In Fashion & Lifestyle Marketing. With Deep Industry
+              Connections And A Passion For Aesthetics, We Place Your Brand At
+              The Center.
+            </p>
+          </div>
+        </Link>
+      </CardContent>
+    </Card>
   );
 }
--- app/[locale]/(influencer)/influencer/(pages)/account-settings/_components/profile-summary-card.tsx
+++ app/[locale]/(influencer)/influencer/(pages)/account-settings/_components/profile-summary-card.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import {   Music, AlertCircle, LogOut } from "lucide-react";
+import { Music, AlertCircle } from "lucide-react";
 import { FaInstagram, FaYoutube } from "react-icons/fa6";
 
 export default function ProfileSummaryCard() {
   return (
-    <div className="rounded-2xl bg-linear-to-r from-[#5E7F3A] to-[#7FA35A] text-white flex items-center justify-center p-4 ">
-      <div className="flex items-center justify-between xl:px-4 w-full xl:max-w-xl px-3 md:px-4 overflow-y-scroll no-scrollbar">
+    <div className="rounded-2xl bg-linear-to-r from-Primary to-light-green text-white flex sm:items-center sm:justify-center p-4 ">
+      <div className="flex flex-col sm:flex-row gap-4 items-center justify-between xl:px-4 w-full xl:max-w-xl px-3 md:px-4 overflow-y-scroll no-scrollbar">
         {/* Left: Avatar + Info */}
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4 w-full">
           <div className="w-20 h-20 rounded-full bg-white border-2 border-white/70 overflow-hidden">
             {/* API image later */}
           </div>
@@ -30,34 +30,33 @@ export default function ProfileSummaryCard() {
         </div>
 
         {/* Right: Socials + Logout */}
-        <div className="flex flex-col items-end gap-4">
+        <div className="flex flex-col gap-4 w-full pl-3 sm:pl-0">
           {/* Social handles */}
-          <div className="space-y-2 text-sm text-white/90">
+          <div className="space-y-2">
             <div className="flex items-center gap-2">
-              <span className="w-5 h-5 bg-white/20 rounded-md flex items-center justify-center">
-                <FaInstagram className="w-3.5 h-3.5" />
+              <span>
+                <FaInstagram size={26}/>
               </span>
               <span>@haniaa_amir</span>
             </div>
 
             <div className="flex items-center gap-2">
-              <span className="w-5 h-5 bg-white/20 rounded-md flex items-center justify-center">
-                <FaYoutube className="w-3.5 h-3.5" />
+              <span>
+                <FaYoutube size={26}/>
               </span>
               <span>hania</span>
             </div>
 
             <div className="flex items-center gap-2">
-              <span className="w-5 h-5 bg-white/20 rounded-md flex items-center justify-center">
-                <Music className="w-3.5 h-3.5" />
+              <span>
+                <Music size={26}/>
               </span>
               <span>@its_haniaa</span>
             </div>
           </div>
 
           {/* Logout */}
-          <button className="px-6 py-1.5 rounded-lg bg-[#F1F6DE] text-[#2D5016] text-sm font-medium hover:opacity-90 flex items-center gap-2">
-            <LogOut className="w-4 h-4" />
+          <button className="px-6 py-1.5 rounded-lg bg-[#F1F6DE] text-[#2D5016] text-sm font-medium hover:opacity-90 gap-2 sm:max-w-44">
             Log Out
           </button>
         </div>
