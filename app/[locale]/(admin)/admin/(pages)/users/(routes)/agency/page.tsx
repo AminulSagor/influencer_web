440a351cda19fa11b2808da5499866dc4297cffd
ninjastorm24 <siamabdullah1100@gmail.com>
2026-01-06 18:15:54 +0600

    user data

---
+++ app/[locale]/(admin)/admin/(pages)/users/(routes)/agency/page.tsx
@@ -0,0 +1,15 @@
+import VerificationBreadcrumb from "../../_components/verification-breadcrumb";
+import VariantLinksCard from "../../_components/variants-links-card";
+import UserCard from "../../_components/user-card";
+
+const page = () => {
+  return (
+    <div className="p-4 space-y-4">
+      <VerificationBreadcrumb type="influencer" name="Hania amir" />
+      <VariantLinksCard />
+      <UserCard />
+    </div>
+  );
+};
+
+export default page;
