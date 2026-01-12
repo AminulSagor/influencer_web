2854579d71db8f2eb0a41314af322214753773c3
ninjastorm24 <siamabdullah1100@gmail.com>
2026-01-12 16:30:44 +0600

    fix

--- app/[locale]/(admin)/admin/(pages)/users/(routes)/brand/page.tsx
+++ app/[locale]/(admin)/admin/(pages)/users/(routes)/brand/page.tsx
@@ -1,13 +1,11 @@
 import VerificationBreadcrumb from "../../_components/verification-breadcrumb";
 import VariantLinksCard from "../../_components/variants-links-card";
-import UserCard from "../../_components/user-card";
 
 const page = () => {
   return (
     <div className="p-4 space-y-4">
       <VerificationBreadcrumb type="influencer" name="Hania amir" />
       <VariantLinksCard />
-    
     </div>
   );
 };
--- app/[locale]/(admin)/admin/(pages)/users/(routes)/influencer/page.tsx
+++ app/[locale]/(admin)/admin/(pages)/users/(routes)/influencer/page.tsx
@@ -5,15 +5,7 @@ import { headers } from "next/headers";
 import { userData } from "../../_components/user-data";
 
 const page = async () => {
-  const headerList = await headers();
-  const pathname = headerList.get("x-current-path");
-  type UserType = keyof typeof userData;
-  const lastSegment = pathname?.split("/").pop();
-  const userType: UserType = (
-    lastSegment && lastSegment in userData ? lastSegment : "influencer"
-  ) as UserType;
-
-  const data = userData[userType];
+  const data = userData["influencer"];
 
   return (
     <div className="p-4 space-y-4">
