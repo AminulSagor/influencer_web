afa7145ce658666871cbceb06a7b433eba823168
Shek Rasel <swe.rasel@gmail.com>
2026-03-07 22:38:31 +0600

    response type define

---
+++ types/service-response.ts
@@ -0,0 +1,12 @@
+export type PaginationMeta = {
+  total: number;
+  page: number;
+  limit: number;
+  totalPages?: number;
+};
+export type ServiceResponse<T, M = undefined> = {
+  success: boolean;
+  data: T;
+  meta?: M;
+  message?: string;
+};
