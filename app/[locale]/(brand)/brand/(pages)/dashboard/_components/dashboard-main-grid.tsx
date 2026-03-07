ecf3cb63aec9c273d7585cca8f0efce214a47f41
Shek Rasel <swe.rasel@gmail.com>
2026-03-07 22:43:30 +0600

    server side api implement

--- app/[locale]/(brand)/brand/(pages)/dashboard/_components/dashboard-main-grid.tsx
+++ app/[locale]/(brand)/brand/(pages)/dashboard/_components/dashboard-main-grid.tsx
@@ -1,15 +1,16 @@
-"use client";
-
 import WorkInProgressCard from "./work-in-progress-card";
 import UpcomingDeadline from "./upcoming-deadlines";
 import ActionRequiredCard from "./action-required-card";
+import { getActiveCampaigns } from "@/service/client/campaigns/active-campaigns";
 
-export default function DashboardMainGrid() {
+export default async function DashboardMainGrid() {
+  const campaigns = await getActiveCampaigns();
+
   return (
     <div className="pt-6 px-4 space-y-4">
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-6">
         <div className="space-y-4 lg:col-span-4">
-          <WorkInProgressCard />
+          <WorkInProgressCard data={campaigns} />
         </div>
 
         <div className="space-y-4 lg:col-span-2">
