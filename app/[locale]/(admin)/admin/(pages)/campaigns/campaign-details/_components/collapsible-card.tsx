d4aaf4f6f1edfa642cc2bc8ab3b74a3e3ec596bf
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2026-03-16 16:22:52 +0600

    The status changed from completed to paid, partial paid, declined

--- app/[locale]/(admin)/admin/(pages)/campaigns/campaign-details/_components/collapsible-card.tsx
+++ app/[locale]/(admin)/admin/(pages)/campaigns/campaign-details/_components/collapsible-card.tsx
@@ -24,11 +24,27 @@ type CollapsibleCardProps = {
 };
 
 const badgeVariants: Record<BadgeType, string> = {
-  Completed: "bg-Primary text-white",
-  "In Review": "bg-orange text-white",
-  Paid: "bg-Primary text-white",
-  "Partial Paid": "bg-Primary text-white",
-  Declined: "bg-red text-white",
+  Completed: "bg-Primary text-white border-0",
+  "In Review": "bg-orange text-white border-0",
+  Paid: "bg-Primary text-white border-0",
+  "Partial Paid": "bg-orange text-white border-0",
+  Declined: "bg-red text-black border-0",
+};
+
+const cardVariants: Record<BadgeType, string> = {
+  Completed: "border border-[#B7C997] bg-[#F8F8EF]",
+  "In Review": "border border-[#F0C998] ",
+  Paid: "border border-[#B7C997] bg-[#F8F8EF]",
+  "Partial Paid": "border border-[#F0C998]",
+  Declined: "border border-[#FF8F8F] bg-[#FFF1F1]",
+};
+
+const headingVariants: Record<BadgeType, string> = {
+  Completed: "text-Primary",
+  "In Review": "text-[#D6852D]",
+  Paid: "text-Primary",
+  "Partial Paid": "text-[#D6852D]",
+  Declined: "text-[#FF1E1E]",
 };
 
 const CollapsibleCard = ({
@@ -39,15 +55,19 @@ const CollapsibleCard = ({
   badge,
 }: CollapsibleCardProps) => {
   const badgeClass = badge ? badgeVariants[badge] || "bg-gray-300" : "";
+  const cardClass = badge ? cardVariants[badge] || "" : "border border-[#DDDDDD]";
+  const headingClass = badge ? headingVariants[badge] || "text-Primary" : "text-Primary";
 
   return (
-    <Card>
+    <Card className={cardClass}>
       <div className="px-4">
         <Accordion type="single" collapsible defaultValue="item-1">
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="mb-4 p-0 font-semibold text-Primary hover:cursor-pointer hover:no-underline">
+          <AccordionItem value="item-1" className="border-b-0">
+            <AccordionTrigger
+              className={`mb-4 p-0 font-semibold hover:cursor-pointer hover:no-underline ${headingClass}`}
+            >
               <div className="flex items-center gap-4 text-md">
-                {icon && <span className="text-Primary">{icon}</span>}
+                {icon && <span className={headingClass}>{icon}</span>}
                 {heading}
                 {badge && <Badge className={badgeClass}>{badge}</Badge>}
                 {badgeText && (
